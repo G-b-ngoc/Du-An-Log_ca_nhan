e01a023509cf180b8c265b414961d483dc6dae1b
Giàng Bảo Ngọc <137142411+G-b-ngoc@users.noreply.github.com>
2024-07-19 10:28:58 +0700

    Wed Cá Nhân

---
+++ Dự-Án_01/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById("loginButton").addEventListener("click", () => {
+    // Chuyển hướng sang trang mới
+    window.location.href = "index.html"; // Thay 'newpage.html' bằng đường dẫn đến trang bạn muốn chuyển tới
+  });
+});
